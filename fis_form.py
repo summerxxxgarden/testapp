f88ce800a77c493cef7bb3f16833a90b5fac996c
summerxxxgarden <109530842+summerxxxgarden@users.noreply.github.com>
2022-08-13 22:30:17 +0300

    Update fis_form.py

--- fis_form.py
+++ fis_form.py
@@ -3,38 +3,61 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import streamlit as st
 
-data1=[]
+st.title("Экспорт дверей с сайта fis51.ru")
 
-for p in range(1,6):
-    url=f'https://fis51.ru/catalog/2?page={p}#top'
-    r=requests.get(url)
-    soup=BeautifulSoup(r.text,'lxml')
-    frond_doors=soup.findAll('div', class_='col-xl-4 col-lg-6 col-md-6 col-12 doors_list_item doors_list_item_2')
+@st.cache
+def f_doors_export():
+    data1=[]
+    #получаем айдишники и названия дверей
+    st.write("Начинаем работу")
+    for p in range(1,6):
+        url=f'https://fis51.ru/catalog/2?page={p}#top'
+        r=requests.get(url)
+        soup=BeautifulSoup(r.text,'lxml')
+        frond_doors=soup.findAll('div', class_='col-xl-4 col-lg-6 col-md-6 col-12 doors_list_item doors_list_item_2')
 
-    for frond_door in frond_doors:
-        frond_door_id = int(frond_door.find('a').get('href')[-3:].replace('/',''))
-        frond_door_title = frond_door.find('strong').text
-        data1.append([frond_door_id,frond_door_title])
+        for frond_door in frond_doors:
+            frond_door_id = int(frond_door.find('a').get('href')[-3:].replace('/',''))
+            frond_door_title = frond_door.find('strong').text
+            data1.append([frond_door_id,frond_door_title])
+        st.write(f"Собраны данные со страницы №{p}")
+            
+    df1 = pd.DataFrame(data1)
+    door_ids=list(df1[0])
+
+    #получаем описания дверей 
+    data2=[]
+    st.write(f"Всего дверей - {len(door_ids)}")
+    persentage_of_doors=0
+    for door_id in door_ids:
+        url=f'https://fis51.ru/item/{door_id}'
+        r=requests.get(url)
+        soup=BeautifulSoup(r.text,'lxml')
+
+        door_description=soup.find('div', id='dop_parm').text
+        door_id_item=door_id
         
-df1 = pd.DataFrame(data1)
-door_ids=list(df1[0])
+        data2.append([door_id_item,door_description])
+        persentage_of_doors+=door_id
+        
+    st.write("Подготовка данных")
+    df2=pd.DataFrame(data2)
 
-data2=[]
+    df1.columns=['id','title1']
+    df2.columns=['id','description']
 
-for door_id in door_ids:
-    url=f'https://fis51.ru/item/{door_id}'
-    r=requests.get(url)
-    soup=BeautifulSoup(r.text,'lxml')
+    df_description=df1.merge(df2,how='left',on='id')
+    st.write("Готово!")
+    return df_description
 
-    door_description=soup.find('div', id='dop_parm').text
-    door_id_item=door_id
-    
-    data2.append([door_id_item,door_description])
-    
-df2=pd.DataFrame(data2)
+@st.cache
+def convert_df(df):
+  # IMPORTANT: Cache the conversion to prevent computation on every rerun
+  return df.to_csv().encode('utf-8')
 
-df1.columns=['id','title1']
-df2.columns=['id','description']
-
-df_description=df1.merge(df2,how='left',on='id')
-df_description
+clicked_download=st.download_button(
+    label="Download data as CSV",
+    data=convert_df(f_doors_export()),
+    file_name='экспорт дверей.csv',
+    mime='text/csv',
+)
