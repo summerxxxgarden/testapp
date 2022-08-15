3fe9f26ec3006da692e33d6e5ca2c539ec6cfee7
summerxxxgarden <109530842+summerxxxgarden@users.noreply.github.com>
2022-08-15 11:16:19 +0300

    Update fis_form.py

--- fis_form.py
+++ fis_form.py
@@ -5,7 +5,9 @@ st.title("Импорт товаров с сайта gigi.ru")
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import os,requests,vk_api
+import os,
+import requests,
+import vk_api
 
 def main():
     
